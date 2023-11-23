af724955bbed59681a3cf481ef340ec2ccfdb7b5
awbhum <andywb707@gmail.com>
2023-11-23 12:38:21 +0000

    shorter fahr2sel.c

---
+++ chapter1/1.3-fahr2cel.c
@@ -0,0 +1,14 @@
+/* see ../LICENSE file for copyright and license details. */
+
+#include <stdio.h>
+
+/* print a table of conversions between fahrenheit and celsius */
+int main(void) {
+    int fahr;
+
+    /* column header */
+    printf("  F\t     C\n");
+
+    for (fahr = 0; fahr <= 256; fahr+=8)
+        printf("%3d\t%6.1f\n", fahr, (5.0/9.0) * (fahr - 32.0));
+}
