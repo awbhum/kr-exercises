ecb0852f3756ef585f290393e5e033125f336ec3
awbhum <andywb707@gmail.com>
2023-11-23 11:24:49 +0000

    exercise 1-1/1-2 hello world program

---
+++ chapter1/1.1-hello.c
@@ -0,0 +1,10 @@
+/* see ../LICENSE file for copyright and license details. */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello,\t");
+    printf("World");
+    printf("!\n");
+    return 0;
+}
